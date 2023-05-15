b8964fbc4dc2bdc29e52b4277c49718a3c29055a
swethavasudevaiah123 <132126078+swethavasudevaiah123@users.noreply.github.com>
2023-05-15 23:48:06 +0530

    Update and rename Degen.sol to Token.sol

--- Token.sol
+++ Token.sol
@@ -5,15 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Degen is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Degen", "DGN") {}
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Solidity", "SOL") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
-
-
-    function redeem(uint256 amt) public{
-        _burn(msg.sender, amt);
     }
 }
